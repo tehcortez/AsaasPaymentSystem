6178c5078b29949c03b332543c6dcac5e22cb3a5
lucas cortez <lucas.cortez@w3americas.com>
2024-03-22 03:30:58 -0300

    add required fields to user model and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('id_asaas')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cpf')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
